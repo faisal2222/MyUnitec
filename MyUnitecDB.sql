17399cfba3cfe7068976a0366b47a2d8d25a277f
Mahmoud Mahmoud <mmahmoud@aut.ac.nz>
2016-11-06 19:40:12 +1300

    Added grades table and timetable data

--- MyUnitecDB.sql
+++ MyUnitecDB.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE  IF NOT EXISTS `myunitecdb` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `myunitecdb`;
--- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 5.6.17, for osx10.6 (i386)
 --
 -- Host: localhost    Database: myunitecdb
 -- ------------------------------------------------------
--- Server version	5.7.14-log
+-- Server version	5.6.24
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,6 +16,38 @@ USE `myunitecdb`;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `grades`
+--
+
+DROP TABLE IF EXISTS `grades`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `grades` (
+  `assesment` varchar(100) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `moduleid` int(11) NOT NULL,
+  `semester` int(11) NOT NULL,
+  `year` int(11) NOT NULL,
+  `grade` varchar(10) DEFAULT '00.00',
+  PRIMARY KEY (`assesment`,`username`,`moduleid`,`semester`,`year`),
+  KEY `gradeFK1_idx` (`moduleid`),
+  KEY `gradeFK2_idx` (`username`),
+  KEY `GradesFK1_idx` (`username`,`moduleid`,`semester`,`year`),
+  CONSTRAINT `GradesFK1` FOREIGN KEY (`username`, `moduleid`, `semester`, `year`) REFERENCES `moduleenrollment` (`username`, `moduleid`, `semester`, `year`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `grades`
+--
+
+LOCK TABLES `grades` WRITE;
+/*!40000 ALTER TABLE `grades` DISABLE KEYS */;
+INSERT INTO `grades` VALUES ('Assignment 1','hampton',5420,2,2016,'86.50'),('Assignment 2','hampton',5420,2,2016,'93.70');
+/*!40000 ALTER TABLE `grades` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `module`
@@ -53,7 +85,6 @@ CREATE TABLE `moduleenrollment` (
   `moduleid` int(11) NOT NULL,
   `semester` int(11) NOT NULL,
   `year` int(11) NOT NULL,
-  `grade` decimal(2,0) DEFAULT NULL,
   `status` varchar(100) NOT NULL,
   PRIMARY KEY (`username`,`moduleid`,`semester`,`year`),
   KEY `modulefk4_idx` (`moduleid`),
@@ -68,7 +99,7 @@ CREATE TABLE `moduleenrollment` (
 
 LOCK TABLES `moduleenrollment` WRITE;
 /*!40000 ALTER TABLE `moduleenrollment` DISABLE KEYS */;
-INSERT INTO `moduleenrollment` VALUES ('hampton',5420,2,2016,0,'enrolled'),('hampton',5430,2,2016,0,'enrolled');
+INSERT INTO `moduleenrollment` VALUES ('hampton',5420,2,2016,'enrolled'),('hampton',5420,2,2017,'pending'),('hampton',5430,2,2016,'enrolled');
 /*!40000 ALTER TABLE `moduleenrollment` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -94,6 +125,7 @@ CREATE TABLE `moduletimetable` (
 
 LOCK TABLES `moduletimetable` WRITE;
 /*!40000 ALTER TABLE `moduletimetable` DISABLE KEYS */;
+INSERT INTO `moduletimetable` VALUES (5420,2,0);
 /*!40000 ALTER TABLE `moduletimetable` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -117,7 +149,7 @@ CREATE TABLE `programme` (
 
 LOCK TABLES `programme` WRITE;
 /*!40000 ALTER TABLE `programme` DISABLE KEYS */;
-INSERT INTO `programme` VALUES (111,'Bacholer of IT'),(222,'Bacholer of business');
+INSERT INTO `programme` VALUES (111,'Bacholer of IT'),(222,'Bacholer of business'),(333,'Bachelor of Networking');
 /*!40000 ALTER TABLE `programme` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -147,7 +179,7 @@ CREATE TABLE `programmeenrollment` (
 
 LOCK TABLES `programmeenrollment` WRITE;
 /*!40000 ALTER TABLE `programmeenrollment` DISABLE KEYS */;
-INSERT INTO `programmeenrollment` VALUES ('hampton',111,'24/2/2014',NULL,'enrolled'),('hampton',222,'24/2/2014',NULL,'enrolled'),('testuser',111,'27/2/2015',NULL,'enrolled');
+INSERT INTO `programmeenrollment` VALUES ('hampton',111,'24/2/2014',NULL,'enrolled'),('hampton',222,'24/2/2014',NULL,'enrolled'),('hampton',333,'27/2/2015',NULL,'enrolled'),('testuser',111,'27/2/2015',NULL,'enrolled');
 /*!40000 ALTER TABLE `programmeenrollment` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -204,6 +236,7 @@ CREATE TABLE `timetableentry` (
 
 LOCK TABLES `timetableentry` WRITE;
 /*!40000 ALTER TABLE `timetableentry` DISABLE KEYS */;
+INSERT INTO `timetableentry` VALUES ('sunday','G105','13:00,14:00',2,2016,5420);
 /*!40000 ALTER TABLE `timetableentry` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -243,4 +276,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-11-05  3:47:58
+-- Dump completed on 2016-11-06 19:39:24
